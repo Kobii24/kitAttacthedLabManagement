0208ca88ed1d25f21e8632ef91290845fed3901e
Kobii24 <quochuyho220603@gmail.com>
2024-09-14 16:17:43 +0700

    add data folder to project

---
+++ src/KitAttachedLab.Data/Repository/InventoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using KitAttachedLab.Data.Base;
+using KitAttachedLab.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitAttachedLab.Data.Repository
+{
+    public class InventoryRepository : GenericRepository<Inventory>
+    {
+        public InventoryRepository(prn231Context context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ src/KitAttachedLab.Data/Repository/WarehouseRepository.cs
@@ -0,0 +1,18 @@
+﻿using KitAttachedLab.Data.Base;
+using KitAttachedLab.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitAttachedLab.Data.Repository
+{
+    public class WarehouseRepository : GenericRepository<Warehouse>
+    {
+        public WarehouseRepository(prn231Context context)
+        {
+            _context = context;
+        }
+    }
+}
